8f009392ddd7a2cb7794848576535db37b7b6ada
ricardotulio <ledo.tulio@gmail.com>
2024-07-28 22:57:57 +0100

    Debugging environment variables

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Start the Spring Boot application
+echo "Spring Datasource: $SPRING_DATASOURCE_URL" >> /home/ec2-user/variables.log
+echo "Postgres User: $POSTGRES_USER" >> /home/ec2-user/variables.log
+echo "Postgres Password: $POSTGRES_PASSWORD" >> /home/ec2-user/variables.log
+
 APP_JAR="/home/ec2-user/ministore-api/target/app.jar"
 nohup java -jar $APP_JAR > /home/ec2-user/ministore-api.log 2>&1 &
