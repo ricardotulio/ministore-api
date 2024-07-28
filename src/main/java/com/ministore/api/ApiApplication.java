dce5f65666b6eaf6c9b689377c0980b237c833fc
ricardotulio <ledo.tulio@gmail.com>
2024-07-28 18:52:57 +0100

    Add /health path to application

--- src/main/java/com/ministore/api/ApiApplication.java
+++ src/main/java/com/ministore/api/ApiApplication.java
@@ -2,12 +2,19 @@ package com.ministore.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@GetMapping("/health")
+	public static String healthCheck()
+	{
+		return "{result: 'ok'}"; 
 	}
 
 }
